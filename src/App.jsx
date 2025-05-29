062963e14260d2a42e1bd9f830fec906d1b2638a
thuramoe <thuramoe@brycenmyanmar.com.mm>
2025-05-29 16:06:40 +0700

    add bootstrap styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-
-import './App.css'
-import AddExpense from './components/expenses/AddExpense'
-import ListExpense from './components/expenses/ListExpense'
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
+import AddExpense from "./components/expenses/AddExpense";
+import ListExpense from "./components/expenses/ListExpense";
 
 function App() {
-
-  return (
-    <>
-      <AddExpense />
-      <br/>
-      <ListExpense />
-    </>
-  )
+	return (
+		<>
+			<Container>
+				<AddExpense />
+				<br />
+				<ListExpense />
+			</Container>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/expenses/ListExpense.jsx
+++ src/components/expenses/ListExpense.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import app from "../../firebaseConfig";
 import { getDatabase, ref, get, remove } from "firebase/database";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
 
 const ListExpense = () => {
 	let [expenses, setExpenses] = useState([]);
@@ -15,51 +18,61 @@ const ListExpense = () => {
 		const snapshot = await get(expenseRef);
 		if (snapshot.exists) {
 			console.log(snapshot.val());
-            const expenses = Object.entries(snapshot.val()).map(([key, value]) => ({
-                key: key,
-                ...value
-            }));
-            console.log(expenses);
+			const expenses = Object.entries(snapshot.val()).map(
+				([key, value]) => ({
+					key: key,
+					...value,
+				})
+			);
+			console.log(expenses);
 			setExpenses(expenses);
 		} else {
 			alert("No data available");
 		}
 	};
 
-    const deleteHandler = async (key) => {
-        const db = getDatabase(app);
-        const expenseRef = ref(db, `expenses/daily-expenses/${key}`);
-        await remove(expenseRef, null);
-        getExpenses();
-    }
+	const deleteHandler = async (key) => {
+		const db = getDatabase(app);
+		const expenseRef = ref(db, `expenses/daily-expenses/${key}`);
+		await remove(expenseRef, null);
+		getExpenses();
+	};
 
 	return (
-		<div>
-			<h3>List of Expenses</h3>
-			<table>
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Amount</th>
-						<th>Action</th>
-					</tr>
-				</thead>
-				<tbody>
-					{expenses.map((expense) => {
-						return (
-							<tr key={expense.key}>
-								<td>{expense.name}</td>
-								<td>{expense.amount}</td>
-								<td>
-									<button>Edit</button>
-									<button onClick={() => deleteHandler(expense.key)}>Delete</button>
-								</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
-		</div>
+		<Row>
+			<Col xs={12}>
+				<h3>List of Expenses</h3>
+				<Table striped bordered hover>
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Amount</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						{expenses.map((expense) => {
+							return (
+								<tr key={expense.key}>
+									<td>{expense.name}</td>
+									<td>{expense.amount}</td>
+									<td>
+										<button>Edit</button>
+										<button
+											onClick={() =>
+												deleteHandler(expense.key)
+											}
+										>
+											Delete
+										</button>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</Table>
+			</Col>
+		</Row>
 	);
 };
 
